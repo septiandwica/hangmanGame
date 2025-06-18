ddfdf6d09afdd8e3294e6369386dc41a0e7bf595
septiandwica <septiandwica03@gmail.com>
2025-06-18 10:52:06 +0700

    commit

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -707,7 +707,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63281" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63492" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
